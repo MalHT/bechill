eab4018f4e22e2ccc4e7aa47b6ef0a1d675be7fa
Mal <me@mal.im>
2016-04-23 13:31:37 +0100

    startCoordinator and connectCoordinator functions

--- bechill.js
+++ bechill.js
@@ -1,15 +1,38 @@
 var apikey = "pcylravfak1kbj4i";
 
-var peer = new Peer(prompt("Your ID"), {key: apikey}); 
-
-var conn = peer.connect(prompt("Connect to"));
-conn.on('open', function(){
-  conn.send('hi!');
+//var peer = new Peer(prompt("Your ID"), {key: apikey});
+var peer = new Peer({
+    key: apikey
 });
 
-peer.on('connection', function(conn) {
-  conn.on('data', function(data){
-    // Will print 'hi!'
-    console.log(data);
-  });
-});
+// Start a new server/host
+var startCoordinator = function () {
+
+    peer.on('connection', function (conn) {
+        conn.on('data', function (data) {
+            
+            console.log(data);
+            
+        });
+    });
+
+};
+
+// Connect to a server/host by its id
+var connectCoordinator = function (serverid) {
+
+    var conn = peer.connect(serverid);
+
+    conn.on('open', function () {
+        conn.send('hi!');
+    });
+
+}
+
+if (window.location.hash.startsWith("#room:")) {
+
+    var serverid = window.location.hash.substr(6);
+
+    connectCoordinator(serverid);
+
+}

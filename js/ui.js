d1bed62609b43702f13ccb05c7dbd20455ab1b08
Cat Flynn <mewtwo97@hotmail.com>
2016-04-23 16:26:20 +0100

    implement send status

--- js/ui.js
+++ js/ui.js
@@ -1,12 +1,21 @@
 var ViewModel = function () {
     var self = this;
-    
-    self.isPlaying = ko.observable(false);
-    self.currentSongName = ko.observable("none");
-    
-    self.togglePlayPause = function () {
+
+    self.isPlaying = ko.observable(currentStatus.playing);
+    self.currentSong = ko.computed(function () {
+        return playlist[currentStatus.song].title;
+    }, this);
+
+    self.playButton = function () {
         self.isPlaying(!self.isPlaying());
+        
+        var copy = copyObject(currentStatus);
+        copy.playing = self.isPlaying();
+        
+        sendUpdatedMediaStatus(copy);
     };
+    
+    
 };
 
 ko.applyBindings(new ViewModel());

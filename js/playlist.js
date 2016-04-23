c70763131380d92e24d8e31a5d43333d3f5a6e55
Cat Flynn <mewtwo97@hotmail.com>
2016-04-23 16:40:19 +0100

    attach play button to backend

--- js/playlist.js
+++ js/playlist.js
@@ -1,22 +1,12 @@
 // List of songs
 var playlist = [
-    {
-        title: "Song Title"
-        , youtube_src: "baSe64/Id"
-    }
-    , {
-        title: "Song Title 2"
-        , youtube_src: "baSe64/Id"
-    }
+    {title: "Song Title", youtube_src: "baSe64/Id"},
+    {title: "Song Title 2", youtube_src: "baSe64/Id"}
 ]
 
 // Status of currently playing song
 var currentStatus = {
-    song: 0, // reference to currently playing song in playlist
-    position: 60000, // timestamp in ms
-    playing: false // play | pause
+    song: 0,      // reference to currently playing song in playlist
+    position: 60000,        // timestamp in ms
+    playing: false           // play | pause
 }
-
-var copyObject = function (thing) {
-    return JSON.parse(JSON.stringify(thing));
-};
--- js/ui.js
+++ js/ui.js
@@ -5,17 +5,22 @@ var ViewModel = function () {
     self.currentSong = ko.computed(function () {
         return playlist[currentStatus.song].title;
     }, this);
+    self.displayStartHostButton = ko.computed(function () {
+        return !isCoordinator && !isConnectedAsClient;
+    }, this);
 
     self.playButton = function () {
         self.isPlaying(!self.isPlaying());
-        
+
         var copy = copyObject(currentStatus);
         copy.playing = self.isPlaying();
         
         sendUpdatedMediaStatus(copy);
     };
-    
-    
+
+    self.startCoordinator = function () {
+        startCoordinator();
+    };
 };
 
 ko.applyBindings(new ViewModel());
